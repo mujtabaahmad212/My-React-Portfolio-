977ae4a711d71f81d22f63bd238c9148d44cc4c6
mujtaba ahmad <mujtabaahmad4200@example.com>
2025-03-17 11:22:54 +0500

    remove unused dependencies and update Home route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Home from "./Components/Home";
-import AboutMe from "./Components/AboutMe";
-import Services from "./Components/Services";
-import Projects from "./Components/Projects";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Contact from "./Components/Contact";
-
+import Home from "./Components/Home";
+import AboutMe from "./Components/AboutMe";
+import Services from "./Components/Services";
+import Projects from "./Components/Projects";
 function App() {
   return (
     <>
@@ -15,7 +14,7 @@ function App() {
       <Navbar className="  " />
      
       <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/services" element={<Services />} />
           <Route path="/Projects" element={<Projects />} />
