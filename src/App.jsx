a1927bba22f00cf6119bd6c0e7956992adadb1a8
mujtaba ahmad <mujtabaahmad4200@example.com>
2025-04-20 11:55:08 +0500

    routing error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
 
@@ -10,19 +10,22 @@ import Services from "./Components/Services.jsx";
 import Projects from "./Components/Projects.jsx";
 function App() {
   return (
-    <>
+    
       <div className="">
       <Navbar className="  " />
-     
+    
       <Routes>
+
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/services" element={<Services />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+       
       </div>
-    </>
+    
+   
   );
 }
 
